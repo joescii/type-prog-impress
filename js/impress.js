c2cd99d67db9a0dffb31f56a6a2ac0ef03e6c047
Joe Barnes <barnesjd@gmail.com>
2014-03-30 13:26:19 -0500

    Got comparison views rolling

--- js/impress.js
+++ js/impress.js
@@ -840,12 +840,4 @@
 (function ( document, window, $ ) {
   'use strict';
 
-  document.addEventListener("keyup", function ( event ) {
-    if ( event.keyCode === 65 ) {
-      $('.all').toggleClass('active');
-      event.preventDefault();
-    }
-  }, false);
-
-
 })(document, window, jQuery);
