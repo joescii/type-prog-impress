5adad683c5a2a3244dfcb93e3b6de786684c7c34
Joe Barnes <barnesjd@gmail.com>
2015-02-28 14:22:12 -0600

    Refactored the Scala snippet to prep for a diff snippet

--- src/main/scala/code/snippet/Scala.scala
+++ src/main/scala/code/snippet/Scala.scala
@@ -6,27 +6,27 @@ import net.liftweb.util.Helpers._
 import scala.xml.NodeSeq
 
 object Scala {
-  def render(in:NodeSeq):NodeSeq = {
-    val src = (for {
-      name <- S.attr("src")
-      code <- LiftRules.loadResourceAsString(s"/scala/$name.scala")
-    } yield { code })
-      .openOr("// Source not found!!!")
+  private def src(attr:String) = (for {
+    name <- S.attr(attr)
+    code <- LiftRules.loadResourceAsString(s"/scala/$name.scala")
+  } yield { code })
+    .openOr("// Source not found!!!")
 
+  private def html(src:String, bg:String) = <pre>
+    <code class={s"scala $bg"}>
+      <div class="centered">{src}</div>
+    </code>
+  </pre>
+
+  private def append(html:NodeSeq) = "* *+" #> html
+
+  def code(in:NodeSeq):NodeSeq = {
     val bg = in.headOption
       .map(_ \ "@data-bg")
       .map(_.toString)
       .map(bg => if(bg contains "sc") "light" else "dark")
       .headOption.getOrElse("light")
 
-    val html = <pre>
-      <code class={s"scala $bg"}>
-        <div class="centered">{src}</div>
-      </code>
-    </pre>
-
-    val transform = "* *+" #> html
-
-    transform(in)
+    append(html(src("src"), bg))(in)
   }
 }
