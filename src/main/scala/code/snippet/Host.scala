a119b4ee6140d5dd1ab070f7fcaa880681086d49
Joe Barnes <barnesjd@gmail.com>
2015-03-15 22:01:22 -0500

    Updating the title page display of the hostname

--- src/main/scala/code/snippet/Host.scala
+++ src/main/scala/code/snippet/Host.scala
@@ -3,8 +3,15 @@ package code.snippet
 import net.liftweb.util.Helpers._
 import java.net.InetAddress
 import net.liftweb.http.S
-import net.liftweb.util.{ClearNodes, PassThru}
+import net.liftweb.util.{Props, ClearNodes, PassThru}
 
 object Host {
-  def render = "*" #> InetAddress.getLocalHost.getHostAddress
+  val name = {
+    val localhost = "localhost:8080"
+    val url = Props.get("url", "localhost:8080")
+    // This lets it run at localhost while showing the IP so everyone can have the endpoint
+    if(url == localhost) InetAddress.getLocalHost.getHostAddress + ":8080"
+    else url
+  }
+  def render = "*" #> name
 }
