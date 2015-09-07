16e4cdc8ef75eb2e346f959f2fe3579565f9d8be
Joe Barnes <barnesjd@gmail.com>
2015-09-07 16:06:09 -0500

    Added build up to type list

--- src/main/scala/com.joescii.typeprog/Vector.scala
+++ src/main/scala/com.joescii.typeprog/Vector.scala
@@ -3,25 +3,29 @@ package com.joescii.typeprog
 import Nat._
 
 sealed trait Vector[Size <: Nat] {
-  def size:Int
-  def ::(head:Int):Vector[NatN[Size]] = NonEmptyVector(head, this)
+  def ::(head:Int):Vector[NatN[Size]] =
+    NonEmptyVector(head, this)
   def +(that:Vector[Size]):Vector[Size]
-  def ++[ThatSize <: Nat](that:Vector[ThatSize]):Vector[Size + ThatSize]
+  def ++[ThatSize <: Nat](that:Vector[ThatSize])
+    :Vector[Size + ThatSize]
 }
 
 case object VNil extends Vector[Nat0] {
-  val size = 0
   def +(that:Vector[Nat0]) = this
   def ++[ThatSize <: Nat](that:Vector[ThatSize]) = that
 }
 
-case class NonEmptyVector[TailSize <: Nat](head:Int, tail:Vector[TailSize]) extends Vector[NatN[TailSize]] {
+case class NonEmptyVector[TailSize <: Nat]
+  (head:Int, tail:Vector[TailSize])
+  extends Vector[NatN[TailSize]]
+{
   type Size = NatN[TailSize]
-  val size = 1 + tail.size
   def +(that:Vector[Size]) = {
     that match {
-      case NonEmptyVector(head2, tail2) => NonEmptyVector(head + head2, tail + tail2)
+      case NonEmptyVector(head2, tail2) =>
+        NonEmptyVector(head + head2, tail + tail2)
     }
   }
-  def ++[ThatSize <: Nat](that:Vector[ThatSize]) = NonEmptyVector(head, tail ++ that)
+  def ++[ThatSize <: Nat](that:Vector[ThatSize]) =
+    NonEmptyVector(head, tail ++ that)
 }
